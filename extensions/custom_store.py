b851b0903727eedf4442f2cc872844658fdad491
Bevywise Networks <support@bevywise.com>
2022-04-07 15:15:26 +0530

    Custom Store
    Custom Store added

---
+++ extensions/custom_store.py
@@ -0,0 +1,31 @@
+global db_cursor
+#
+# elastic_search cursor
+#
+global elastic_search
+import os, sys
+global datasend
+
+custom_store.py
+global Client_obj
+sys.path.append(os.getcwd()+’/../extensions’)
+
+# Called on the initial call to set the SQL Connector
+
+def setsqlconnector(conf):
+
+global db_cursor
+db_cursor=conf[“sql”]
+# Called on the initial call to set the Elastic Search Connector
+
+def setelasticconnector(conf):
+global elastic_search
+elastic_search=conf[“elastic”]
+def setwebsocketport(conf):
+global web_socket
+web_socket=conf[“websocket”]
+
+def setclientobj(obj):
+global Client_obj
+Client_obj=obj[‘Client_obj’]
+#Client_ob
